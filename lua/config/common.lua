b4e3d78fe5f3f99602d91fcbe095e57f084a45c5
Alexander Makeenkov <amakeenk@altlinux.org>
2024-10-05 23:47:27 +0300

    init

---
+++ lua/config/common.lua
@@ -0,0 +1,21 @@
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.mouse = a
+vim.opt.mousefocus = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.laststatus = 2
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.clipboard = "unnamedplus"
+
+
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
